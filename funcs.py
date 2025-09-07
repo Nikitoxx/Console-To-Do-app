0f9876a67e5fb6e6b8f0abed593876c7f66e8118
Nikita Raylean <nekithis@gmail.com>
2025-09-07 11:30:02 +0300

    Added new function

--- funcs.py
+++ funcs.py
@@ -4,18 +4,24 @@ def show_menu():
 1.Show tasks
 2.Finished tasks
 3.Add new task
-4.Exit\n''')
+4.To assign a completed task
+5.Exit\n''')
 
 
 def show_tasks():
     a = open('tasks.txt', 'r')
-    
     for i in a:
         print(i, end='')
-        
-    a.close() 
-
-
-show_tasks()
-
+    a.close()
+    
+    
+def show_finish_task():
+    a = open('tasks.txt', 'r')
+    for i in a:
+        if '(done)' in i:
+            b = i.replace('(done)', '')
+            print(b)
+    a.close()
+    
+show_finish_task()
 
